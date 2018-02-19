33b8fe18353a08f51fa2de3aadd2d83a03968ce8
Anthony Bisulco <bisulco.a@husky.neu.edu>
2018-02-18 19:20:47 -0500

    Python multiprocessing version

--- bio.cpp
+++ bio.cpp
@@ -4,6 +4,9 @@
 #include <math.h>
 #include <stdint.h>
 #include <string>
+#include <array>
+#include <fstream>
+#include <stdio.h>
 using namespace std;
 
 typedef struct Float3
@@ -16,7 +19,7 @@ typedef struct Float3
 float my_nextafterf(float a, int dir);
 float onemove_in_cube(float3 *p0, float3 *v, float *htime, int *id);
 void printFloat(float3 *pos, string name);
-void readArr(float (*x)[208][256][225]);
+void readArr(float *x);
 int main()
 {
     const uint8_t Nx = 208;                                                             //Number of voxels in x direction of target
@@ -30,15 +33,14 @@ int main()
     const float3 orginOffset = {(-Mx * D) / 2 + (Nx / 2), (-My * D) / 2 + (Ny / 2), 0}; //offset for orgin of detector
     const float3 ep = {Nx / 2, Ny / 2, H};                                              //location of detector
     const float muBone = 0.573;                                                         //mu for bone in scan
-    const float muFat = 0.193;                                                          //my for fat in scan
-    float mu[Nx][Ny][Nz];                                                               //Mu array of voxels
+    const float muFat = 0.193;  
+    float* mu=(float*)malloc(sizeof(float)*Nx*Ny*Nz);                                                     //my for fat in scan
     float detector[Mx][My];                                                             //Array of detector pixels
     float3 pos;                                                                         //position vector
     float3 dir;                                                                         //direction vector
     uint8_t i, j;                                                                       //loop counters for unrolled loop
     float L;                                                                            //energy count
     readArr(mu); 
-    //cout<<mu[0][0][0];
     for (int ii = 0; ii < Mx * My; ii = ii + 1)
     {
         i = ii / Mx;
@@ -65,17 +67,22 @@ int main()
         */
     }
 }
+void readArr(float *x){
+  string line;
+  ifstream myfile ("headct.txt");
+  int w,y,z;
+  if (myfile.is_open())
+  {
+    while ( getline (myfile,line) )
+    {
+        fscanf(line, " (%d,%d,%d)", &w, &y,&z);
+    }
+    cout<<w<<y<<z;
+    myfile.close();
+  }
 
-void readArr(float (*x)[256][225]){
-    cout<<"Prefault I"<<endl;
-    for(int i=0;i<208;i=i+1){
-    for(int j=0;j<256;j=j+1){
-	for(int z=0;z<225;z=z+1){
-	x[i][j][z]=0;
-	}
-	}
-	}
-    //cout<<"x(0,0,0) "<<x[0][0][0]<<endl;
+	//x[i*208+j*256+z]=1;
+    //cout<<"x(0,0,0) "<<x[0]<<endl;
 }
 
 void printFloat(float3 *pos, string name)
