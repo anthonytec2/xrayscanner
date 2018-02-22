8a440120ec3b41b84c807d1faab24389c7f1a5b1
Anthony Bisulco <bisulco.a@husky.neu.edu>
2018-02-22 09:26:06 -0500

    added in error checking at end, changed datafile, removed other src and profiled code

--- bio.py
+++ bio.py
@@ -1,16 +1,25 @@
+import warnings
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore",category=FutureWarning)
+    import h5py
 import numpy as np
-import h5py   
 import math
 import matplotlib.pyplot as plt  
-import move_step
+try:
+    profile  # throws an exception when profile isn't defined
+except NameError:
+    profile = lambda x: x
+
 @profile
-def onemove_in_cube_true(p0,v):
+def onemove_in_cube_true(p0,v):   
+    v[v==0]=1e-16
     htime=np.abs((np.floor(p0)-p0+(v>0))/v,dtype='float32')
     minLoc=htime.argmin()
     dist=htime[minLoc]
     htime=p0+dist*v
     htime[minLoc]=round(htime[minLoc])+np.spacing(abs(htime[minLoc]))*np.sign(v[minLoc])
     return htime, dist
+
 @profile
 def main():
     Nx = 208
@@ -31,6 +40,8 @@ def main():
     f = h5py.File('headct.h5', 'r')
     headct=np.array(f.get('ct'))
     headct=np.transpose(headct)
+    det=f.get('det')
+    det=np.transpose(det)
     mu=np.zeros((Nx,Ny,Nz),dtype='float32')
     detector=np.zeros((Mx,My),dtype='float32')
     mu[np.nonzero(headct>0)]=((headct[np.nonzero(headct>0)]-0.3)/(0.7))*(muBone-muFat)+muFat
@@ -48,8 +59,8 @@ def main():
             pos,dist=onemove_in_cube_true(pos,dir)
             if 0 <= pos[0] < Nx and 0<=pos[1]<Ny  and h<=pos[2] < h+Nz:
                 L=L*np.exp(-1*mu[math.floor(pos[0]),math.floor(pos[1]),math.floor(pos[2]-h)]*dist)
-        detector[i][j] = L;  
-
+        detector[i][j] = L;
+    print(np.isclose(det,detector,rtol=.5).all())
 
 if __name__ == '__main__':
     main()
