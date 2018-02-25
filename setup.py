84f835a306958dc57f06fc09fd463ae0952ae7e6
Anthony Bisulco <bisulco.a@husky.neu.edu>
2018-02-25 09:37:03 -0500

    changed profile names, added in -03 march native flags for compliation

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from distutils.core import setup
+from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy
 
+
+extensions = [ Extension("move_step", ["move_step.pyx"], extra_compile_args=["-O3"," -march=native"],extra_link_args=["-O3"," -march=native"])]
+
 setup(
     name="Cython Fast Cube App",
-    ext_modules=cythonize('move_step.pyx'),
+    ext_modules=cythonize(extensions),
     include_dirs=[numpy.get_include()],
 )
